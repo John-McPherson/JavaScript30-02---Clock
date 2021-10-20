8884dcd2c80061c4033a365698bc4ef99c6e10b8
John-McPherson <john-mac1989@hotmail.co.uk>
2021-10-20 18:39:25 +0000

    tidy up js and add transition style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,18 @@
-let time = new Date();
-let hours = time.getHours() * 30;
-hours = hours += 90;
-let minutes = time.getMinutes() * 6;
-minutes = minutes += 90;
-let sec = time.getSeconds() * 6;
-sec = sec += 90;
-
-
-function hour() {
+function setDate(){
+    let time = new Date();
+    let hours = time.getHours()*30 +90;
+    let sec = time.getSeconds()*6 +90;
+    let minutes = time.getMinutes()*6 + 90;
     document.getElementsByClassName('hour-hand')[0].style.transform = `rotate(${hours}deg)`;
-}
-
-function minute() {
     document.getElementsByClassName('min-hand')[0].style.transform = `rotate(${minutes}deg)`;
-}
-
-function seconds() {
     document.getElementsByClassName('second-hand')[0].style.transform = `rotate(${sec}deg)`;
 }
-hour();
-minute();
-seconds();
+setDate();
+setInterval(setDate,1000);
 
-window.setInterval(function () {
-    hour();
-    minute();
-    seconds();
-}, 1000);
+
+
+
+
+
+
