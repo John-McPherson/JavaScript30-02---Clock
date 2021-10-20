a2968cab13846b1c40874b5e120ac4c7e4cc1023
John-McPherson <john-mac1989@hotmail.co.uk>
2021-10-20 17:23:46 +0000

    add js and css to min hand

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,16 @@ console.log(time.getMinutes());
 console.log(time.getSeconds());
 let hours = time.getHours()*30;
 hours = hours += 90;
-console.log(hours);
+let minutes = time.getMinutes()*6;
+minutes = minutes += 90;
+console.log(minutes);
 
 function hour(){
      document.getElementsByClassName('hour-hand')[0].style.transform = `rotate(${hours}deg)`;
 }
+function minute(){
+    document.getElementsByClassName('min-hand')[0].style.transform = `rotate(${minutes}deg)`;
+}
+
 hour();
+minute();
